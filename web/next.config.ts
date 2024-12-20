1f23f9dc02f3cbfd25b8adec994fdac74fdf45a8
CatTimothy <cat1984@duck.com>
2024-12-21 00:59:03 +0800

    Proxy API server to web server

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+
+const rewrites = async (): Promise<
+  | { source: string; destination: string }[]
+  | { beforeFiles: { source: string; destination: string }[]; afterFiles: { source: string; destination: string }[]; fallback: { source: string; destination: string }[] }
+> => {
+  return [
+    {
+      source: "/convert",
+      destination: "http://localhost:5000/convert",
+    },
+  ];
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  rewrites,
 };
 
 export default nextConfig;
